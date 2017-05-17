41a4a1ea551b1097e002e690aa96dd53ac3e84da
Robert Boone <robert@rlb3.com>
2017-05-17 10:31:49 -0500

    Add url name

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :todo, Todo.Web.Endpoint,
   on_init: {Todo.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "pacific-escarpment-73840.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
